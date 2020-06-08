d07555f48ed625bdef69972d65f2b549978b1cb4
nirav7707 <56816580+nirav7707@users.noreply.github.com>
2020-06-08 15:07:40 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# reactdashbord
-Created with CodeSandbox
---
+++ src/Component/MainComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container-fluied">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="row" />
+              <div className="row" />
+            </div>
+            <div className="col-md-8" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
